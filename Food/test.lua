a4fe31785bd194c4cebbb0742c0f7b91be926452
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-17 14:12:42 -0300

    GoFish fish food update for test version v27.1.1

--- Food/test.lua
+++ Food/test.lua
@@ -1,5 +1,5 @@
 local name = "test"
-local version = "27.1.0"
+local version = "27.1.1"
 
 food = {
     name = name,
@@ -9,38 +9,25 @@ food = {
     version = version,
     packages = {
         {
-            os = "linux",
-            arch = "amd64",
-            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.0/test_27.1.0_linux_amd64",
-            sha256 = "b4d52247d032a2a2e3dc0ba38da85368f511a8b73d09f241464401193a3b8385",
-            resources = {
-                {
-                    path = "test_27.1.0_linux_amd64",
-                    installpath = "bin/test",
-                    executable = true
-                },
-            }
-        },
-        {
             os = "windows",
             arch = "arm64",
-            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.0/test_27.1.0_windows_arm64.exe",
-            sha256 = "2cce7eb47e37ccc0047217619af7e99b3758cace841525c266a403a7af1707a4",
+            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.1/test_27.1.1_windows_arm64.exe",
+            sha256 = "798ea8d0c14666b1612c835b3ad6839dd217767c44185e162f6f20d542f67821",
             resources = {
                 {
-                    path = "test_27.1.0_windows_arm64.exe",
+                    path = "test_27.1.1_windows_arm64.exe",
                     installpath = "bin\\test.exe",
                 },
             }
         },
         {
             os = "linux",
-            arch = "arm64",
-            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.0/test_27.1.0_linux_arm64",
-            sha256 = "91d56d412106f97d2ea6ef9cea5a4f88bc736fa79e56050fb3aa019252e6bbc8",
+            arch = "amd64",
+            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.1/test_27.1.1_linux_amd64",
+            sha256 = "703f12562582e03ddde518a49508a10dbd7795309ae25a522dce273e51f30217",
             resources = {
                 {
-                    path = "test_27.1.0_linux_arm64",
+                    path = "test_27.1.1_linux_amd64",
                     installpath = "bin/test",
                     executable = true
                 },
@@ -49,11 +36,11 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.0/test_27.1.0_windows_amd64.exe",
-            sha256 = "6f16461c3b00b5c4b3fecc7ba8a16c6b3ec7c1c3967afe35009a902c9eaef145",
+            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.1/test_27.1.1_windows_amd64.exe",
+            sha256 = "0b46e4427bddb8b438ed3d1de70e1db32574b43169a0188a6e0d400a7faf4be9",
             resources = {
                 {
-                    path = "test_27.1.0_windows_amd64.exe",
+                    path = "test_27.1.1_windows_amd64.exe",
                     installpath = "bin\\test.exe",
                 },
             }
@@ -61,11 +48,11 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.0/test_27.1.0_darwin_all",
-            sha256 = "bc45f6b604bebbd119c7adca768eee3a04fa94d307176748603963fdd1a32311",
+            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.1/test_27.1.1_darwin_all",
+            sha256 = "454d423b25b6ea81addb3df4c89b2270fa8c79cbb4205b2a0a14429a37e0eb40",
             resources = {
                 {
-                    path = "test_27.1.0_darwin_all",
+                    path = "test_27.1.1_darwin_all",
                     installpath = "bin/test",
                     executable = true
                 },
@@ -74,11 +61,24 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.0/test_27.1.0_darwin_all",
-            sha256 = "bc45f6b604bebbd119c7adca768eee3a04fa94d307176748603963fdd1a32311",
+            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.1/test_27.1.1_darwin_all",
+            sha256 = "454d423b25b6ea81addb3df4c89b2270fa8c79cbb4205b2a0a14429a37e0eb40",
             resources = {
                 {
-                    path = "test_27.1.0_darwin_all",
+                    path = "test_27.1.1_darwin_all",
+                    installpath = "bin/test",
+                    executable = true
+                },
+            }
+        },
+        {
+            os = "linux",
+            arch = "arm64",
+            url = "https://github.com/caarlos0-graveyard/test/releases/download/v27.1.1/test_27.1.1_linux_arm64",
+            sha256 = "bb12badfea8eba86b86536a2a1a0e2f818de24d23de688b7d69af5aab5783bf6",
+            resources = {
+                {
+                    path = "test_27.1.1_linux_arm64",
                     installpath = "bin/test",
                     executable = true
                 },
