8b9e853f38816c2c9e6ae2fedb67c81edf971c41
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-05-02 02:55:21 +0000

    meh
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- foo/main.go
+++ foo/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	version "github.com/caarlos0/go-version"
+)
+
+const art = ` _____
+|  ___|__   ___
+| |_ / _ \ / _ \
+|  _| (_) | (_) |
+|_|  \___/ \___/
+`
+
 func main() {
-	println("foo")
+	fmt.Println(version.GetVersionInfo(
+		version.WithASCIIName(art),
+		version.WithBuiltBy("goreleaser"),
+		version.WithAppDetails("foo", "foo is a test", "https://github.com/caarlos0"),
+	).String())
 }
