6dab4cb0ae3097a3c5ea65e7c63447e02125bfd4
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-03-09 14:31:10 -0300

    wip
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- fury.sh
+++ fury.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-if [ "${1: -4}" == ".deb" ] || [ "${1: -4}" == ".rpm" ]; then
-	cd dist
-	echo "uploading $1"
-	curl -f -q -s -F package="@$1" "https://$FURY_TOKEN@push.fury.io/caarlos0/" >/dev/null
+if [ "${1: -4}" == ".deb" ] || [ "${1: -4}" == ".rpm" ] || [ "${1: -4}" == ".apk" ]; then
+  cd dist
+  echo "uploading $1"
+  curl -f -q -s -F package="@$1" "https://$FURY_TOKEN@push.fury.io/caarlos0/" >/dev/null
 fi
